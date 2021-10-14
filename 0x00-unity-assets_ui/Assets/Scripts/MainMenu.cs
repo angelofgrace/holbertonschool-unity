17595e908a15bb20a6fc49a839f04484039500de
Grace Fallon <graceleefallon@gmail.com>
2021-10-13 19:56:16 -0500

    Main menu level buttons connected to scene management script

---
+++ 0x00-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+}
